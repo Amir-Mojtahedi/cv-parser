2dfbacbc03a7eb0a959bf34aef21065beff62c21
Amir Mojtahedi <amirrezamojtahedi2@gmail.com>
2025-06-24 11:23:09 -0400

    Fix lint problems

--- src/app/(cv-matcher)/dashboard/page.tsx
+++ src/app/(cv-matcher)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type React from "react";
-import { useState } from "react";
 import { Upload, FileText, Briefcase, Users } from "lucide-react";
 import { Button } from "@/components/ui/radix-components/button";
 import {
